63b55db215e536122eb72c044b9733f4a9f0e1f6
FranzCSUF <fsalvador79@gmail.com>
2018-07-03 10:20:23 -0700

    Define a function that takes a cart item and returns a Bootstrap-styled DOM tree that includes the item name, brand, price, and image.#5

--- main.js
+++ main.js
@@ -265,3 +265,63 @@ function view(viewName) {
     renderDetails.removeAttribute('class', 'hidden')
   }
 }
+
+function cartView(item) {
+
+  var container = document.createElement('div')
+  var row = document.createElement('div')
+  var firstCol = document.createElement('div')
+  var img = document.createElement('img')
+  var secondCol = document.createElement('div')
+  var title = document.createElement('h5')
+  var subTitle = document.createElement('h6')
+  var desc = document.createElement('p')
+  var price = document.createElement('p')
+
+  container.setAttribute('class', 'container')
+  container.setAttribute('id', 'details')
+  container.style.backgroundColor = 'white'
+  row.setAttribute('class', 'row')
+  firstCol.setAttribute('class', 'col col-lg-4')
+  firstCol.style.marginTop = '50px'
+  img.setAttribute('class', 'card-img-top')
+  img.setAttribute('src', 'src=.../1--px180/')
+  secondCol.setAttribute('class', 'col')
+  secondCol.style.padding = '50px'
+  secondCol.style.textAlign = 'left'
+  title.setAttribute('class', 'card-title')
+  title.style.fontSize = '36pt'
+  subTitle.setAttribute('class', 'card-subtitle mb-2 text-muted')
+  subTitle.style.fontSize = '24pt'
+  desc.setAttribute('class', 'card-text')
+  price.setAttribute('class', 'card-text')
+  price.style.paddingTop = '20px'
+  price.style.fontSize = '18pt'
+
+  img.src = item['imageUrl']
+  title.textContent = item['name']
+  subTitle.textContent = item['brand']
+  desc.textContent = item['details']
+  price.textContent = item['price']
+
+  container.appendChild(row)
+  row.appendChild(firstCol)
+  firstCol.appendChild(img)
+  row.appendChild(secondCol)
+  secondCol.appendChild(title)
+  secondCol.appendChild(subTitle)
+  secondCol.appendChild(desc)
+  secondCol.appendChild(price)
+
+  return container
+
+}
+
+function buildCart(cart) {
+  var $cartItems = document.createElement('div')
+  for (var i = 0; i < cart.items.length; i++) {
+    var cartItem = cartView(cart.items[i])
+    $cartItems.appendChild(cartItem)
+  }
+  return $cartItems
+}
